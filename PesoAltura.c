d90c9b8742cdd1f287a6bab465c6f1ced4989981
Rafael-RobertoS <rafaelalemaosilva@gmail.com>
2024-09-04 14:36:28 +0000

    muito

--- PesoAltura.c
+++ PesoAltura.c
@@ -9,5 +9,17 @@ int main(){
     pessoa.peso = 82;
     pessoa.altura = 172;
     printf("Peso: %i\n Altura: %i\n" , pessoa.peso, pessoa.altura);
+
+    if (pessoas.altura> 110){
+        printf ( "Pode se divertir \n")
+    }
+    else {
+        ´printf("Vá embora!\n");
+
+    }
+
+    printf ("%i\n",(int) sizeof )
+
     return 0;
+
 }
